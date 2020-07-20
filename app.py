fce03ee08e4ba9e73432272bb5d143ae59c62bd7
heeyeon Kim <wbslqj0434@gmail.com>
2020-07-20 17:13:08 +0900

    list for step 1

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
-from flask import Flask ,render_template #flask 라이브러리 가져옴
-
-app = Flask(__name__)  #app이라는 flask객체를 생성 (이 안에는 member field와 method가 포함 되어있다)
+from flask import Flask ,render_template
+from data import Articles
+app = Flask(__name__)
 app.debug=True
-
 
 @app.route('/')
 def index():
     print("Success")
     # return "TEST"
-    return render_template('home.html', hello="GaryKim")
+    return render_template('home.html',hello="GaryKim")
 
 @app.route('/about')
 def about():
     print("Success")
     # return "TEST"
-    return render_template('about.html', hello="GaryKim")
+    return render_template('about.html',hello="GaryKim")
 
 @app.route('/articles')
 def articles():
     print("Success")
     # return "TEST"
-    return render_template('articles.html', hello="GaryKim")
+    articles = Articles()
+    print(len(articles))
+    return render_template('articles.html',articles=articles)
 
 
 if __name__=='__main__':      #서버 띄우는 명칭
