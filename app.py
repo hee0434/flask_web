94d174a716b240b7f69834aba0ba9dc2a5b65338
heeyeon Kim <wbslqj0434@gmail.com>
2020-07-21 11:47:50 +0900

    params step 1

--- app.py
+++ app.py
@@ -29,6 +29,12 @@ def show_image():
     return render_template('image.html')
 
 
+@app.route('/article/<int:id>')
+def article(id):
+    print(id)
+    return render_template(article.html)
+
+
 if __name__=='__main__':      #서버 띄우는 명칭
     #app.run(host='0.0.0.0', port='8080')      #빈칸이면 5000
     app.run()
